8d493efedf85ae12e081a22bfa7576da7d9206d8
Bryan De Jesús Mares Barrientos <BryanMaresDev@outlook.com>
2022-06-13 23:37:12 -0500

    Añadiendo funcionalidades a MesasView.vue y más

--- server/routes/ordenes.js
+++ server/routes/ordenes.js
@@ -17,6 +17,24 @@ router.get("/todas_las_ordenes", async (req, res) => {
     return;
 });
 
+router.post('/nueva_orden', async(req, res) => {
+    try{
+        const body = req.body;
+        const query =   'INSERT INTO orden ' +
+                        '(ord_mesa_id, ord_mes_id, ord_estado, ord_fecha, ord_productos, ord_tot) ' +
+                        'VALUES (?,?,?,now(),?,?)';
+        await connection.query(query, [body.ord_mesa_id, body.ord_mes_id, body.ord_estado, null, null]);
+        res.json('ok');
+    } catch(error){
+        res.json({ 
+            error:error
+        });
+    }
+});
+
+
+
+
 router.delete('/eliminar_orden', async (req,res) => {
     try{
         const ord_id = req.body.ord_id;
